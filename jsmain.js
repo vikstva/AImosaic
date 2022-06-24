a045fc3ec8973f6f87b19fe8b539ce9422ff2625
vikstva <vikstva@gmail.com>
2022-06-24 07:18:22 +0300

    scroll to top

---
+++ jsmain.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.scrollup').click(function() {
+    $("html, body").animate({
+      scrollTop:0
+    },1000);
+  })
+})
+$(window).scroll(function() {
+  if ($(this).scrollTop()>200) {
+    $('.scrollup').fadeIn();
+  }
+  else {
+    $('.scrollup').fadeOut();
+  }
+});
