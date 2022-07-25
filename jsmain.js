e62835fce03d6dbcb85cda7fe8232f81cf19d6a1
vikstva <vikstva@gmail.com>
2022-07-25 03:32:22 +0300

    update

--- jsmain.js
+++ jsmain.js
@@ -2,11 +2,12 @@ const content = document.getElementById('content')
 const loader = document.getElementsByClassName('loader')
 document.addEventListener("DOMContentLoaded", function() {
 
-  
   setTimeout(() => {
   content.style.display = 'block'
   loader[0].style.display = 'none'
   }, 1500)
+  
+
 });
 
 $(function() {
@@ -24,17 +25,3 @@ $(window).scroll(function() {
     $('.scrollup').fadeOut();
   }
 });
-
-const slideDelay = 6000;
-
-      const dynamicSlider = document.getElementById("slider");
-
-      var curSlide = 0;
-      window.setInterval(()=>{
-        curSlide++;
-        if (curSlide === dynamicSlider.childElementCount) {
-          curSlide = 0;
-        }
-
-        dynamicSlider.firstElementChild.style.setProperty("margin-left", "-" + curSlide + "00%");
-      }, slideDelay);
